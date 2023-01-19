31e0a72d8d5ee08f5746446258d268587389e3db
whit3-n051e <122798625+whit3-n051e@users.noreply.github.com>
2023-01-20 02:09:29 +0300

    Made a distinction between mobile and desktop

--- src/js/script.js
+++ src/js/script.js
@@ -3,22 +3,38 @@ const isMobile = /iphone|ipad|ipod|android/i.test(navigator.userAgent);
 
 function fromPxVw(s) {return Number(s.slice(0, s.length - 2))};
 function units(n) {return String("calc(var(--unit) *" + n + ")")}
+function unith(n) {return String("calc(var(--unith) *" + n + ")")}
 
 function fromPercent (s) {return Number(s.slice(0, s.length - 1))};
 function toPercent(n) {return String(n) + '%'};
 
-// ------------------------------------------------------------------------
-let UIMultiplier = 1;
-if (isMobile) UIMultiplier = 3.1;
+// const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0) * 0.01;
+// const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0) * 0.01;
 
-document.documentElement.style.setProperty("--unit", UIMultiplier + 'vw');
+const themeSheet = document.getElementById("stylesheetlink-theme");
+const scalesSheet = document.getElementById("stylesheetlink-scales");
+const propertiesSheet = document.getElementById("stylesheetlink-properties");
+
+if (isMobile) {
+    themeSheet.href = "src/css/mobile/theme-m.css";
+    scalesSheet.href = "src/css/mobile/scales-m.css";
+    propertiesSheet.href = "src/css/mobile/properties-m.css";
+}
+
+
+// ------------------------------------------------------------------------
+document.documentElement.style.setProperty("--unit", '1vmax');
 document.documentElement.style.setProperty("--mobile", Number(isMobile));
 // ------------------------------------------------------------------------
+
+
+
 
 const header = document.getElementById("header");
 const headerContainer = document.getElementById("header-container");
 const headerDragger = document.getElementById("header-dragger");
 const headerDraggerArrow = document.getElementById("header-dragger-arrow");
+const linkDescription = document.getElementsByClassName("link-description");
 
 const menuBackgrounds = document.getElementsByClassName("menu-icon-white-bg");
 const menuIcons = document.getElementsByClassName("menu-icon");
@@ -55,6 +71,7 @@ const activateLink = (linkNo) => {
     menuIcons[linkNo].style.filter = "invert(0%)";
     iconLines[linkNo].style.width = units(2);
     iconLines[linkNo].style.marginLeft = 0;
+    linkDescription[linkNo].style.top = units(4);
 }
 
 const deactivateLink = (linkNo) => {
@@ -63,15 +80,12 @@ const deactivateLink = (linkNo) => {
     menuIcons[linkNo].style.filter = "invert(100%)";
     iconLines[linkNo].style.width = 0;
     iconLines[linkNo].style.marginLeft = units(1);
+    linkDescription[linkNo].style.top = units(2);
 }
 
 // Menu links animation
 for (let i = 0; i < menuLinks.length; i++) {
     if (isMobile) {
-        menuLinks[i].onclick = () => {
-            activateLink(i);
-            deactivateLink(i);
-        }
     } else {
         menuLinks[i].onmouseover = () => activateLink(i);
         menuLinks[i].onmouseout = () => deactivateLink(i);
