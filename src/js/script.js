b0e83d6358eac8a44298380f61f418aa039ed738
whit3-n051e <122798625+whit3-n051e@users.noreply.github.com>
2023-01-19 12:59:40 +0300

    Improved header

--- src/js/script.js
+++ src/js/script.js
@@ -6,18 +6,34 @@ const toPx = (n) => {return String(n) + 'px'};
 const fromPercent = (s) => {return Number(s.slice(0, s.length - 1))};
 const toPercent = (n) => {return String(n) + '%'};
 
+// ------------------------------------------------------------------------
+
 const header = document.getElementById("header");
+const headerContainer = document.getElementById("header-container")
+const menuItems = document.getElementsByClassName("menu-icon-square");
+const menuIcons = document.getElementsByClassName("menu-icon");
+const iconLines = document.getElementsByClassName("icon-line");
+const headerDragger = document.getElementById("header-dragger");
+const headerDraggerArrow = document.getElementById("header-dragger-arrow");
 
 if (isMobile) {
     header.style.backgroundColor = "var(--alt-black)"
 } else {
     header.style.width = "auto";
-    header.style.borderBottomLeftRadius = "1.5vw";
+    headerContainer.style.borderBottomLeftRadius = "1.5vw";
+
+    headerDragger.onmouseover = () => {
+        header.style.top = "0vw";
+        headerDraggerArrow.style.transform = "rotate(180deg)";
+    };
+    headerDragger.onmouseout = () => {
+        header.style.top = "-4vw";
+        headerDraggerArrow.style.transform = "rotate(0deg)";
+    };
+    header.onmouseover = headerDragger.onmouseover;
+    header.onmouseout = headerDragger.onmouseout;
 }
 
-const menuItems = document.getElementsByClassName("menu-icon-square");
-const menuIcons = document.getElementsByClassName("menu-icon");
-const iconLines = document.getElementsByClassName("icon-line");
 
 for (let i = 0; i < menuItems.length; i++) {
     menuItems[i].onmouseover = () => {
